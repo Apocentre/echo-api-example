73b7bf49a094ebb24fa0a3aaa6b92307c84e754a
ppoliani <ppoliani@gmail.com>
2024-02-18 14:41:17 +0200

    chore: remove lofs

--- server/router.go
+++ server/router.go
@@ -2,12 +2,10 @@ package server
 
 import (
 	"echo-api-example/endpoints"
-	"fmt"
 
 	"github.com/labstack/echo/v4"
 )
 
 func RegisterRoutes(e *echo.Echo) {
-	fmt.Printf("Inside register routes")
 	e.GET("/", endpoints.HelloWorld)
 }
