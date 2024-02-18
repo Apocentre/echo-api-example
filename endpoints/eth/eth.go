f01352fc201c45ceb0fe1d1387bee0807e68dd7e
ppoliani <ppoliani@gmail.com>
2024-02-18 21:31:39 +0200

    feat: use go routin in the endpoint processor

--- endpoints/eth/eth.go
+++ endpoints/eth/eth.go
@@ -2,7 +2,9 @@ package eth
 
 import (
 	"fmt"
+	"math/big"
 	"net/http"
+	"time"
 
 	"echo-api-example/endpoints"
 
@@ -13,19 +15,45 @@ type QS struct {
   Address string `query:"address"`
 }
 
+type RequestError struct {
+	Status int
+	Msg string
+}
+
+func NewRequestError(status int, msg string) *RequestError {
+	return &RequestError {status, msg}
+}
+
 func GetAddressBalance(c echo.Context) error {
 	fmt.Println("Running...")
+	result_c := make(chan *big.Int, 1)
+	error_c := make(chan *RequestError, 1)
 	ctx := c.(*endpoints.ApiContext)
+	eth_client := ctx.EthClient()
+	
+	go func ()  {
+		time.Sleep(10 * time.Second)
+	
+		var qs QS
+		err := c.Bind(&qs); if err != nil {
+			error_c <- NewRequestError(http.StatusBadRequest, "bad request")
+		}
+	
+		balance, err := eth_client.GetBalance(qs.Address)
+		if err != nil {
+			error_c <- NewRequestError(http.StatusInternalServerError, "internal server error")
+		}	
 
-	var qs QS
-	err := c.Bind(&qs); if err != nil {
-    return c.String(http.StatusBadRequest, "bad request")
+		result_c <- balance
+	}()
+	
+	select {
+	case balance := <- result_c:
+		return ctx.String(http.StatusOK, balance.String())
+	case req_error := <- error_c:
+		return c.String(req_error.Status, req_error.Msg)
+	case <- c.Request().Context().Done():
+		return nil
 	}
-
-	balance, err := ctx.EthClient().GetBalance(qs.Address)
-	if err != nil {
-		return err
-	}
-
-	return ctx.String(http.StatusOK, balance.String())
+	
 }
