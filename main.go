6fafbeb7e24fb83fbbf0463d191067b6fd47b24a
ppoliani <ppoliani@gmail.com>
2024-02-18 16:30:14 +0200

    feat: pass the correct eth rpc to the eth client

--- main.go
+++ main.go
@@ -4,14 +4,12 @@ import (
 	"echo-api-example/blockchain"
 	"echo-api-example/server"
 	"echo-api-example/utils"
-	"fmt"
 )
 
 func main() {
 	utils.InitEnvConfigs()
-	fmt.Printf(">>>>>> %v", utils.EnvConfigs.ETH_rpc)
 	
-	client, err := blockchain.NewEthClient("TODO_ADD_RPC_URL")
+	client, err := blockchain.NewEthClient(utils.EnvConfigs.ETH_rpc)
 	if err != nil {
 		panic("Eth client cannot be created")
 	}
